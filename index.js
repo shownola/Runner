d8c05f09fe732240c95a29aa805be2ac1d51f99a
shownola <swasieko@gmail.com>
2020-02-04 12:19:11 -0700

    Add 'lodash.debounce' to fix issues with the add event

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env nodemon
 
+const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 
+const start = debounce(() => {
+  console.log('STARTING PROGRAM');
+}, 100);
+
 chokidar.watch('.')
-  .on('add', () => console.log('START USERS PROGRAM'))
+  .on('add', start) 
   .on('change', () => console.log('FILE CHANGED'))
   .on('unlink', () => console.log('FILE UNLINKED'));
