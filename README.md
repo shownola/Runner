1a0843cb298e045ec39e07ec23e3a16c43484ce8
shownola <swasieko@gmail.com>
2020-02-04 17:20:48 -0700

    Added caporal and child_process from the standard library

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # README
 * Runner will be a command line tool that will monitor any changes made in you code and will automatically restart your server. This app will be similar to 'nodemon'.
+* To run program either call the program name(runner) or node index.js
+* From terminal call runner -h for help
 
+USAGE:
+* runner [filename]
+ARGUMENTS:
+* [filename]  Name of a file to execute    optional
+
+REQUIRMENTS/ISSUES:
 * Need to detect when a file changes - 'chokidar' package should do this
 * Provide some help to users of the CLI tool - use 'caporal' to build the CLI tool
 * Figure out how to execute some JS code form within a JS program - use the standard library module 'child_process' to execute a program
--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 #!/usr/bin/env nodemon
 
+
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
+const program  = require('caporal');
+const fs       = require('fs');
+const {spawn}  = require('child_process');
 
-const start = debounce(() => {
-  console.log('STARTING PROGRAM');
-}, 100);
 
-chokidar.watch('.')
-  .on('add', start) 
-  .on('change', () => console.log('FILE CHANGED'))
-  .on('unlink', () => console.log('FILE UNLINKED'));
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of file to execute')
+  .action(async ({filename}) => {
+    const name = filename || 'index.js';
+    try {
+      await fs.promises.access(name);
+    } catch(err){
+      throw new Error(`Could not find the file ${name}`);
+    }
+
+    const start = debounce(() => {
+      spawn('node', [name], { stdio: 'inherit'});
+    }, 100);
+
+    chokidar.watch('.')
+      .on('add', start)
+      .on('change', start)
+      .on('unlink', start);
+  });
+
+program.parse(process.argv);
